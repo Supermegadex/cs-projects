71f4b4aaef2b780d902a2b7fea17768a21e8a066
Daniel Noon <delpinothedragon1@hotmail.com>
2018-08-15 16:19:47 -0500

    complete assignment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    String[] messages = {
+      "Hello, world! I'm Daniel.",
+      "",
+      "I like to program in my free time.",
+      "I also act in the Marian Musicals and plays.",
+      "I have three cats: TJ, Jasper, and Draco.",
+      "Check out my projects at https://github.com/Supermegadex"
+    };
+
+    for (String message : messages) {
+      System.out.println(message);
+    }
   }
 }
