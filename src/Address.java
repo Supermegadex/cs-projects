770f9d0641b493e837ecfd260d431e7b4fcc6ec2
Daniel Noon <supercookeis@outlook.com>
2018-11-28 14:34:24 -0600

    Finish Addresses

--- src/Address.java
+++ src/Address.java
@@ -1,15 +1,27 @@
-public class Address {
-  public String name;
-  public String street;
-  public String csz;
+import java.util.Comparator;
+
+public class Address implements Comparator<Address> {
+  private String name;
+  private String lastName;
+  private String street;
+  private String csz;
 
   Address(String name, String street, String csz) {
     this.name = name;
     this.street = street;
     this.csz = csz;
+    String[] names = name.split(" ");
+    this.lastName = names[names.length - 1];
   }
 
-  getAddress() {
+  Address() {}
+
+  String getAddress() {
     return String.format("%s\n%s\n%s", this.name, this.street, this.csz);
+  }
+
+  @Override
+  public int compare(Address o1, Address o2) {
+    return o1.lastName.compareTo(o2.lastName);
   }
 }
--- src/Addresses.java
+++ src/Addresses.java
@@ -1,13 +1,10 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Addresses {
-  public static void main(String[] args) throws FileNotFoundException, IOException {
+  public static void main(String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(
-      new FileReader("C:\\Users\\delpi\\Documents\\cs\\hello-world\\src\\in\\22c_addresses.txt")
+      new FileReader("C:\\Users\\2019194\\Downloads\\projects\\cs-projects\\in\\22c_addresses.txt")
     );
 
     ArrayList<Address> addresses = new ArrayList<>();
@@ -24,18 +21,10 @@ public class Addresses {
       else done = true;
     }
 
-    while (addresses.size() > 0) {
-      int lastName = 100;
-      int index = 0;
-      for (Address addr : addresses) {
-        String[] name = addr.name.split(" ");
-        Utils.log(name[name.length - 1]);
-        Utils.log(String.format("%d", Character.getNumericValue(name[name.length - 1].charAt(0))));
-        // if (name[name.length - 1].charAt(0) < lastName) {
+    addresses.sort(new Address());
 
-        // }
-      }
-      addresses.remove(0);
+    for (Address address : addresses) {
+      Utils.log(address.getAddress() + "\n");
     }
 
     reader.close();
